a44d5b1fc70809cafdf180129bca889fe7e0d2e1
Sidelong <sidelongkepron@gmail.com>
2026-05-22 20:50:28 +0300

    HW32 commit

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -2,13 +2,23 @@ import {useContext} from "react";
 import {ShmitterContext} from "../utils/context.js";
 
 const Avatar = ({size}) => {
-    const {user} = useContext(ShmitterContext);
+    const {user, setUser} = useContext(ShmitterContext);
+
+    const handleClick = () => {
+        if (size !== 'large') return;
+        const newUrl = prompt('Enter new avatar URL:');
+        if (newUrl) {
+            setUser({...user, avatar: newUrl});
+        }
+    }
 
     return (
         <img
             className={`user-avatar ${size ?? ''}`}
             src={user.avatar}
             alt={user.name}
+            onClick={handleClick}
+            style={size === 'large' ? {cursor: 'pointer'} : {}}
         />
     )
 }
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -8,7 +8,7 @@ const Stats = () => {
     return (
         <div className={'user-stats'}>
             <div>
-                <Avatar/>
+                <Avatar size={'large'}/>
                 {user.name}
             </div>
             <div className={'stats'}>
