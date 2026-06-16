2e6e8534afde33c06de28e0ff628f8b4972b5616
Sidelong <sidelongkepron@gmail.com>
2026-06-16 19:12:09 +0300

    HW-39 Commit

--- src/reducer/shmitterReducer.js
+++ src/reducer/shmitterReducer.js
@@ -11,21 +11,12 @@ type State = {
 }
 */
 
-import {CHANGE_AVATAR, CHANGE_NAME} from "../actions/userActions.js";
-import {CHANGE_STATS} from "../actions/statsActions.js";
+import {userReducer} from "./userReducer.js";
+import {statsReducer} from "./statsReducer.js";
 
 export const shmitterReducer = (state, action) => {
-    switch (action.type) {
-        case CHANGE_NAME:
-            return {...state, user: {...state.user, name: action.payload || state.user.name}}
-        case CHANGE_AVATAR:
-            return {...state, user: {...state.user, avatar: action.payload || state.user.avatar}}
-        case CHANGE_STATS: {
-            const res = state.stats[action.payload.statsType] + action.payload.sum;
-            const stats = {...state.stats, [action.payload.statsType]: res < 0 ? 0 : res};
-            return {...state, stats};
-        }
-        default:
-            return state;
+    return {
+        user: userReducer(state.user, action),
+        stats: statsReducer(state.stats, action),
     }
 }
---
+++ src/reducer/userReducer.js
@@ -0,0 +1,12 @@
+import {CHANGE_AVATAR, CHANGE_NAME} from "../actions/userActions.js";
+
+export const userReducer = (state, action) => {
+    switch (action.type) {
+        case CHANGE_NAME:
+            return {...state, name: action.payload || state.name}
+        case CHANGE_AVATAR:
+            return {...state, avatar: action.payload || state.avatar}
+        default:
+            return state;
+    }
+}
